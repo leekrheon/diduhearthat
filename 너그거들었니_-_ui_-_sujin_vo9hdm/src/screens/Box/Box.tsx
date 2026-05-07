482e7d291bb57b86b2af4a883f2a306225a05ee1
leekrheon <juheon1155@naver.com>
2026-05-08 01:01:13 +0900

    Add files via upload

---
+++ 너그거들었니_-_ui_-_sujin_vo9hdm/src/screens/Box/Box.tsx
@@ -0,0 +1,15 @@
+export const Box = (): JSX.Element => {
+  return (
+    <main
+      className="relative w-[1064px] h-[1817px] overflow-hidden"
+      data-model-id="7:23-frame"
+      aria-label="UI preview"
+    >
+      <img
+        className="block w-[1064px] h-[1817px]"
+        alt="Ui sujin"
+        src="https://c.animaapp.com/uxoltzQB/img/ui---sujin.png"
+      />
+    </main>
+  );
+};
